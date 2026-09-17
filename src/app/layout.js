e5d624dad04b010d7107ce10085fe1a22e17dd06
Wanguy Calvert <wanguycalvert92@gmail.com>
2026-09-17 13:34:08 -0400

    Add custom vector SVG favicon for browser tab

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,9 @@ import "./globals.css";
 export const metadata = {
   title: "Town Riddles Online - Jeu Multijoueur Virtuel",
   description: "Rejoignez la ville virtuelle, rencontrez d'autres joueurs, affrontez-les à des mini-jeux et résolvez les 5 maisons de devinettes !",
+  icons: {
+    icon: "/icon.svg",
+  },
 };
 
 export default function RootLayout({ children }) {
